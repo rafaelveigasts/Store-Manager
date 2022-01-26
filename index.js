66eb405f09efeb654ca7e944b4314176174cf51c
rafael veiga <rafaelveig@hotmail.com>
2022-01-26 12:49:45 -0300

    refatora index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+// const rescue = require('express-rescue');
+const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
