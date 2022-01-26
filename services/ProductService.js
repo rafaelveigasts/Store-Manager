6e3d572378e53f9dad01d8b9fa7b96c618d2599f
rafael veiga <rafaelveig@hotmail.com>
2022-01-26 14:31:54 -0300

    validador de produto geral

--- services/ProductService.js
+++ services/ProductService.js
@@ -8,7 +8,10 @@ const validateName = async (name) => {
     return { status: 400, message: '"name" is required' };
   }
   if (productName.lenght < 5) {
-    return { status: 400, message: '"name" must be at least 5 characters long' };
+    return {
+      status: 400,
+      message: '"name" must be at least 5 characters long',
+    };
   }
   if (productName) return { status: 409, message: 'Product already exists' };
 };
@@ -18,12 +21,30 @@ const validateProductQuantity = async (quantity) => {
     return { status: 400, message: '"quantity" is required' };
   }
   if (typeof quantity !== 'number') {
-    return { status: 400, message: '"quantity" must be a numbeer larger then or equal to 1' }
+    return {
+      status: 400,
+      message: '"quantity" must be a numbeer larger then or equal to 1',
+    };
   }
   return { status: 200, message: 'Quantity is Valid' };
+};
+
+const validateProduct = async (req, res, next) => {
+  const { name, quantity } = req.body;
+  const { status: nameStatus, message: nameMessage } = await validateName(name);
+  const { status: quantityStatus, message: quantityMessage } = validateProductQuantity(quantity);
+  if (nameStatus !== 200) {
+    return res.status(nameStatus).json({ message: nameMessage });
+  }
+
+  if (quantityStatus !== 200) {
+    return res.status(quantityStatus).json({ message: quantityMessage });
+  }
+  next();
 };
 
 module.exports = {
   validateName,
   validateProductQuantity,
+  validateProduct,
 };
