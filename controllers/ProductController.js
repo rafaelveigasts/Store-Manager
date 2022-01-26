f679b1010fafef677067aed6017c78da78797aeb
rafael veiga <rafaelveig@hotmail.com>
2022-01-26 13:22:36 -0300

    adiciona model de products com biblioteca Joi

---
+++ controllers/ProductController.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+const Product = require('../services/ProductService');
+
+const createProduct = async (req, res, next) => {
+  const { name, quantity } = req.body;
+  const { error } = Joi.object({
+    name: Joi.string().not().empty().required(),
+    quantity: Joi.number().integer().min(1).required(),
+  }).validate({ name, quantity });
+  if (error) {
+    return next(error);
+  }
+  const newProduct = await Product.createProduct(name, quantity);
+  if (newProduct.error) return next(newProduct.error);
+};
+
+module.exports = {
+  createProduct,
+};
