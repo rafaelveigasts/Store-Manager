1eaa4ab787688284efe073d3088d31403a4a37c3
rafael veiga <rafaelveig@hotmail.com>
2022-01-26 14:23:40 -0300

    validate name e quantity

--- services/ProductService.js
+++ services/ProductService.js
@@ -1,11 +1,29 @@
-const Product = require('../models/ProductModel');
+const ProductModel = require('../models/ProductModel');
 
-const createProduct = async (name, quantity) => {
-  await Product.createProduct(name, quantity);
-    
-  return true;
+const validateName = async (name) => {
+  const allProducts = await ProductModel.getAllProducts();
+  const productName = allProducts.find((product) => product.name === name);
+
+  if (!productName) {
+    return { status: 400, message: '"name" is required' };
+  }
+  if (productName.lenght < 5) {
+    return { status: 400, message: '"name" must be at least 5 characters long' };
+  }
+  if (productName) return { status: 409, message: 'Product already exists' };
+};
+
+const validateProductQuantity = async (quantity) => {
+  if (!quantity || quantity < 1) {
+    return { status: 400, message: '"quantity" is required' };
+  }
+  if (typeof quantity !== 'number') {
+    return { status: 400, message: '"quantity" must be a numbeer larger then or equal to 1' }
+  }
+  return { status: 200, message: 'Quantity is Valid' };
 };
 
 module.exports = {
-  createProduct,
+  validateName,
+  validateProductQuantity,
 };
