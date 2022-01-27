018297a76d3352b7612a16edf6b17845e16ce042
rafael veiga <rafaelveig@hotmail.com>
2022-01-27 18:34:03 -0300

    updateProductById no service com comment

--- services/ProductService.js
+++ services/ProductService.js
@@ -19,10 +19,19 @@ const findProductById = async (id) => {
   return product;
 };
 
+const updateProductById = async (id, name, quantity) => {
+  const updatedProduct = await ProductModel.updateProductById(id, name, quantity);
+  if (!updatedProduct) {
+    return { code: 404, message: { message: 'Product not found' } };
+  }
+  return updatedProduct;
+};
+
 module.exports = {
   createProduct,
   getAllProducts,
   findProductById,
+  updateProductById,
 };
 
 /*  Anotação: como service está uma camada acima do model, ele vai passar as informações a serem adicionadas no DB,
@@ -31,4 +40,6 @@ Sempre passar as funções do model aqui com o mesmo nome, pois é daqui que ela
 Obs.: na função findProductByID definimos duas chaves no controller, uma para o código de erro e outra para a mensagem de erro.
 Essas chaves estão vazias, portanto aqui eu fiz o valor delas. 
 Ou seja, se meu product tiver a propriedade code ou message no controller o service vai identificar e atribuir esses valores a ele.
+
+A função updateProductById segue a mesma lógica do findProductById, porém com a diferença de que ela vai atualizar o produto no bd.
 */
