cba652e8e21bda1e9cf617c9498770854a5255ab
rafael veiga <rafaelveig@hotmail.com>
2022-01-31 12:32:41 -0300

    teste

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -2,26 +2,23 @@ const connection = require('./connection');
 
 const createSale = async () => {
   const [sale] = await connection.execute(
-    'INSERT INTO StoreManager.sales () VALUES ()',
+    'INSERT INTO StoreManager.sales VALUES ()',
   );
   return sale.insertId;
 };
 
-const addProductToSale = async (saleId, productId, quantity) => {
-  const [sale] = await connection.execute(
-    'INSERT INTO StoreManager.sale_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
-    [saleId, productId, quantity],
+const addProductToSales = async (array) => {
+  await connection.execute(
+    'INSERT INTO StoreManager.sales_products VALUES(?)',
+    [array],
   );
-  return sale;
 };
 
 module.exports = {
   createSale,
-  addProductToSale,
+  addProductToSales,
 };
 
 /* 
 A funcção createSale é a responsável por criar uma venda gerando um id para ela.
-
-A funcção addProductToSale é responsável por adicionar um produto a uma venda.
 */
