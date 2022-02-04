23b9b245b0dbb127fb1b62eb3f96e1927c44b4cc
rafael veiga <rafaelveig@hotmail.com>
2022-02-04 10:27:50 -0300

    test model 2

--- test/unit/models.js
+++ test/unit/models.js
@@ -51,5 +51,71 @@ describe('Verifica se o produto é inserido no DB', () => {
     })
   })
 
-  
+  describe('Verifica se um produto é localizado com sucesso', () => {
+    it('Deve retornar um objeto com o produto localizado', async() => {
+      const result = await ProductModel.findById(payload.id);
+      expect(result).to.be.an('object');
+      expect(result).to.have.property('id');
+      expect(result).to.have.property('name');
+      expect(result).to.have.property('quantity');
+    })
+  })
+
+  describe('Verifica se um produto não é localizado', () => {
+    before( async() => {
+      const execute =[[]];
+
+      sinon.stub(ProductModel, 'execute').resolves(execute);
+    })
+
+    after( async()=> {
+      ProductModel.findById.restore();
+    })
+
+    it('Deve retornar um null', async() => {
+      const result = await ProductModel.findById(payload.id);
+     
+      expect(result).to.be.null;
+    })
+  })
+
+  describe('Verifica se a venda é inserida corretamente no DB', () => {
+    const payload = [[1,1,2], [2,1,5]];
+
+    before( async() => {
+      const execute =[{ insertId: 1 }];
+
+      sinon.stub(connection, 'query').resolves(execute);
+    })
+
+    after( async()=> {
+      connection.query.restore();
+    })
+
+    it('Deve retornar um objeto com o id da venda', async() => {
+      const result = await SaleModel.createSalesProducts(payload);
+      expect(result).to.be.an('object');
+      expect(result).to.have.property('id');
+    })
+  })
+
+  describe('Verifica se uma venda é encontrada no DB', () => {
+    before( async () => {
+      sinon.stub(SaleModel, 'getSaleById').resolves([{
+        date: "2021-09-09T04:54:29.000Z",
+        product_id: 1,
+        quantity: 2
+      },
+      {
+        date: "2021-09-09T04:56:29.000Z",
+        product_id: 2,
+        quantity: 5
+      }
+    ]);
+    })
+
+    after( async() => {
+      SaleModel.getSaleById.restore();
+    })
+  })
 })
